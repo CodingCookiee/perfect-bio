ae56127abdc5abd628d9e1a5ffc393885b0e478e
CodingCookiee <rzqutubawan@gmail.com>
2025-03-06 19:39:58 +0500

    custom loading; using radix ui

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,10 +1,37 @@
-export default function Loading() {
+'use client'
+import React, { useEffect, useState } from 'react';
+import { ProgressWithValue } from '@/components/ui/progress-with-value';
+
+const PERCENTAGE = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+const LABELS = ['Just a moment', 'Gathering information', 'Working on it', 'Creating magic', 'Almost done'];
+const Loading = () => {
+  const [index, setIndex] = useState(0);
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    let timer;
+
+    const updateProgress = () => {
+      setProgress(PERCENTAGE[index]);
+
+      if (index < PERCENTAGE.length - 1) {
+        setIndex(index + 1);
+      } else {
+        clearInterval(timer);
+      }
+    };
+
+    timer = setInterval(updateProgress, 1000); 
+
+    return () => clearInterval(timer);
+  }, [index]);
+
   return (
-    <div className="flex items-center justify-center min-h-[60vh]">
-      <div className="relative">
-        <div className="h-24 w-24 rounded-full border-t-4 border-b-4 border-blue-500 animate-spin"></div>
-        <div className="absolute top-0 left-0 h-24 w-24 rounded-full border-t-4 border-b-4 border-blue-200 animate-spin" style={{ animationDirection: 'reverse', animationDuration: '1.5s' }}></div>
-      </div>
+    <div className="w-full space-y-2 px-10">
+      <h2 className="text-lg font-semibold">{LABELS[index]}</h2>
+      <ProgressWithValue value={progress} position="follow" />
     </div>
   );
-}
+};
+
+export default Loading;
