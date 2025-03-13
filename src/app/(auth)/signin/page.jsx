82cd693ec0fdb316b08cdfb910e9c6255f01976a
CodingCookiee <rzqutubawan@gmail.com>
2025-03-13 17:16:52 +0500

    styling signin page

--- src/app/(auth)/signin/page.jsx
+++ src/app/(auth)/signin/page.jsx
@@ -1,35 +1,32 @@
 import React from 'react';
+import { Linkedin } from 'lucide-react';
+import { FaGoogle } from "react-icons/fa";
+import { Button } from "@/components/ui/button"
 
-export default function SignIn() {
+const Page = () => {
   return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-2xl font-bold mb-4">Sign In</h1>
-      <form className="max-w-md mx-auto">
-        <div className="mb-4">
-          <label htmlFor="email" className="block mb-2">Email</label>
-          <input 
-            type="email" 
-            id="email" 
-            className="w-full p-2 border rounded" 
-            placeholder="Enter your email"
-          />
+    <div className='signin h-dvh w-full flex flex-col items-center justify-center p-4'>
+        <h1 className="text-3xl font-audiowide font-semibold mb-8">Sign in</h1>
+      <div className='social-logins flex flex-col items-center justify-center gap-4 w-full max-w-2xs'>
+        
+        <Button className='w-full py-5 cursor-pointer bg-transparent border border-primary
+          text-text hover:bg-primary/20 hover:text-primary transition-all duration-300 ease-in-out
+          flex items-center justify-center gap-2'>
+          <Linkedin className="size-5" /> Sign in with LinkedIn
+        </Button>
+        
+        <Button className='w-full py-5 cursor-pointer bg-transparent border border-primary
+          text-text hover:bg-primary/20 hover:text-primary transition-all duration-300 ease-in-out
+          flex items-center justify-center gap-2'>
+          <FaGoogle className="size-5" /> Sign in with Google
+        </Button>
+        
+        <div className="text-sm text-gray-500 mt-4">
+          We'll never share your information without permission
         </div>
-        <div className="mb-4">
-          <label htmlFor="password" className="block mb-2">Password</label>
-          <input 
-            type="password" 
-            id="password" 
-            className="w-full p-2 border rounded" 
-            placeholder="Enter your password"
-          />
-        </div>
-        <button 
-          type="submit" 
-          className="w-full bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
-        >
-          Sign In
-        </button>
-      </form>
+      </div>
     </div>
   );
 }
+
+export default Page;
