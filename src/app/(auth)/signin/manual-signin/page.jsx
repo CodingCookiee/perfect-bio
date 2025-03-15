a1b7f9cec97529bfd3920bd4eba4e557b609bc79
CodingCookiee <rzqutubawan@gmail.com>
2025-03-15 06:53:47 +0500

    completed manual signin page

--- src/app/(auth)/signin/manual-signin/page.jsx
+++ src/app/(auth)/signin/manual-signin/page.jsx
@@ -1,9 +1,82 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import { Mail, Lock, Eye, EyeOff, EyeClosed } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
+import Image from "next/image";
 
-export default function page() {
+const Page = () => {
+  const [showPassword, setShowPassword] = useState(false);
   return (
-    <div>
-      Manual Signin
+    <div className="signin w-vw h-[90vh] flex flex-col items-center justify-center gap-2.5">
+      <div className=" h-1/10 w-1/3 max-w-xs relative mb-2.5">
+        <Image
+          src="/app/app-icon.png"
+          alt="logo"
+          fill
+          className="object-cover"
+          priority
+        />
+      </div>
+      <form className="flex flex-col items-center justify-center gap-4 w-full max-w-xs">
+        <div className="relative w-full">
+          <Input placeholder="Email" className="w-full py-5 pl-10" />
+          <Mail
+            className="absolute left-3 top-1/2 transform -translate-y-1/2 text-muted-foreground"
+            size={18}
+          />
+        </div>
+
+        <div className="relative w-full">
+          <Input
+            placeholder="Password"
+            className="w-full py-5 pl-10"
+            type={showPassword ? "text" : "password"}
+          />
+          <Lock
+            className="absolute left-3 top-1/2 transform -translate-y-1/2 text-muted-foreground"
+            size={18}
+          />
+          <button
+            type="button"
+            onClick={() => setShowPassword(!showPassword)}
+            className="cursor-pointer absolute right-3 top-1/2 transform -translate-y-1/2 text-muted-foreground"
+          >
+            {showPassword ? <EyeOff size={18} /> : <EyeClosed size={18} />}
+          </button>
+        </div>
+        <div className="w-full flex justify-end -mt-2 mb-1">
+          <Button
+            variant="link"
+            className="text-muted-foreground  p-0 h-auto"
+          >
+            <Link href="/signin/forgot-password" className="text-muted-foreground text-xs text-text">Forgot Password?</Link>
+          </Button>
+        </div>
+        <div className="w-full flex justify-center">
+          <Button
+            variant="default"
+            className="w-full py-6 cursor-pointer bg-primary 
+          text-blue-50 text-md hover:bg-primary/90 transition-all duration-300 ease-in-out
+          flex items-center justify-center gap-2"
+          >
+            Login
+          </Button>
+        </div>
+        <div className="w-full flex items-center justify-center gap-1">
+          <h2 className="text-muted-foreground text-sm">Don't have an account?</h2>
+          <span className="text-muted-foreground ">
+            <Button variant="link" className="p-0 h-auto mb-1">
+              <Link href="/signup" className="text-primary text-md font-semibold">
+                Sign up
+              </Link>
+            </Button>
+          </span>
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
+
+export default Page;
--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -14,12 +14,12 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive font-nunito text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
           outline:
-          "border border-input bg-background font-nunito shadow-xs hover:bg-accent hover:text-accent-foreground hover:!border-primary/50 dark:bg-input/30 dark:border-input dark:hover:bg-input/50 dark:hover:!border-primary",
+          "border border-input bg-background font-nunito shadow-xs hover:bg-accent hover:text-accent-foreground hover:!border-primary/50 dark:bg-input/30 dark:border-input dark:hover:bg-input/50 dark:hover:!border-primary transition-all duration-500 ease-in-out",
         secondary:
           "bg-secondary font-nunito text-secondary-foreground shadow-xs hover:bg-secondary/80",
         ghost:
           "hover:bg-accent font-nunito hover:text-accent-foreground dark:hover:bg-accent/50",
-        link: "text-primary underline-offset-4 hover:underline",
+          link: "text-primary font-nunito  relative no-underline after:absolute after:-bottom-0.5 after:left-0 after:h-[2.5px] after:w-full after:origin-bottom-right after:scale-x-0 after:bg-primary after:transition-transform after:duration-300 after:ease-[cubic-bezier(0.65_0.05_0.36_1)] hover:after:origin-bottom-left hover:after:scale-x-100 transition-all duration-300 ease-in-out",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
